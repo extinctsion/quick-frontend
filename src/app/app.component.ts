9a625d150dc5ef54f2cf7ec2164a1a08bea09ce6
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2024-09-27 20:03:19 +0000

    Added api to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UrlInputComponent } from './url-input/url-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UrlInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'quickurl';
+
+  shortenURL(input: string) {}
 }
---
+++ src/app/url-input/url-input.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UrlInputComponent } from './url-input.component';
+
+describe('UrlInputComponent', () => {
+  let component: UrlInputComponent;
+  let fixture: ComponentFixture<UrlInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UrlInputComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UrlInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
